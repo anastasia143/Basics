a9bd5ef196f4b3c62fb7bd2e4974fe15ba00191b
anastasia143 <semenova.anastasia143@yandex.ru>
2015-11-12 10:48:05 +0300

    Graph2

--- Data structures/Trees and Graphs/graph/graph2.h
+++ Data structures/Trees and Graphs/graph/graph2.h
@@ -4,6 +4,10 @@
 class EdgeNode
 {
 public:
+	EdgeNode(int y, int weight, EdgeNode* next)
+		: y(y)
+		, weight(weight)
+		, next(next) {}
 	int y;
 	int weight;
 	EdgeNode* next;
@@ -12,7 +16,30 @@ public:
 class Graph2
 {
 public:
-	EdgeNode* edges[max]; // list of edges for each vertex
+	Graph2(bool directed)
+		: directed(directed)
+		, nVertices(0)
+		, nEdges(0)
+	{
+		for (int i = 1; i <= max; i++)
+			degree[i] = 0;
+		for (int i = 1; i <= max; i++)
+			edges[i] = NULL;
+	}
+
+	insertEdge(int x, int y, int weight, bool pair)
+	{
+		EdgeNode *p;
+		p = new EdgeNode(y, weight, edges[x]);
+		edges[x] = p;
+		degree[x]++;
+		if (pair)
+			insertEdge(y,x, weight, false);
+		else
+			nEdges++;
+	}
+
+	EdgeNode* edges[max];
 	int degree[max];
 	int nVertices;
 	int nEdges;
